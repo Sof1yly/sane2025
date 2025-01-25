e9156e072802f7163a355d1e86edcbc5ee8cf103
Pupa-Tha <pupa.tha12@gmail.com>
2025-01-25 19:42:53 +0700

    BullShit

--- Assets/Scripts/DialogMang/DialogManager.cs
+++ Assets/Scripts/DialogMang/DialogManager.cs
@@ -1,37 +1,4 @@
-<<<<<<< HEAD
-//using UnityEngine;
 
-//public class DialogManager : MonoBehaviour
-//{
-//    public DialogData dialogData; // Reference to the DialogData ScriptableObject
-
-//    private int currentDialogIndex = 0;
-
-//    private void Start()
-//    {
-//        DisplayDialog();
-//    }
-
-//    public void DisplayDialog()
-//    {
-//        if (currentDialogIndex < dialogData.dialogEntries.Length)
-//        {
-//            var dialogEntry = dialogData.dialogEntries[currentDialogIndex];
-
-//            Debug.Log($"NPC: {dialogEntry.npc.npcName}");
-//            Debug.Log($"Dialog: {dialogEntry.dialogText}");
-//            Debug.Log($"Bubble: {dialogEntry.bubble.bubbleName}");
-
-//            // Proceed to the next dialog line (Example for stepping through dialogs)
-//            currentDialogIndex++;
-//        }
-//        else
-//        {
-//            Debug.Log("End of dialog.");
-//        }
-//    }
-//}
-=======
 using UnityEngine;
 using System.IO;
 
@@ -74,4 +41,4 @@ public class DialogManager : MonoBehaviour
         return null;
     }
 }
->>>>>>> main
+
